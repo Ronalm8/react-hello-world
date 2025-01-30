4e665e3869c9e4547706770228d2dbc98b478d3d
Ronalm8 <ronalbuitrago93@gmail.com>
2025-01-30 16:47:18 -0500

    segundo commit con modificaciones de css y boosttrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-
+import Card  from "./components/Card";
+import Carrusel from "./components/Carrusel";
 
 function App() {
-  return <p>Hola Mundo!</p>;
+  return <><><h1>Hola Mundo!</h1><Card></Card></><Carrusel></Carrusel></>;
 }
 
 export default App;
---
+++ src/components/Carrusel.tsx
@@ -0,0 +1,30 @@
+
+
+function Carrusel() {
+    return (
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+          <img src="src/components/imagen.png" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+          <img src="src/components/radio.png" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+          <img src="src/components/gol.png" className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    );
+  }
+                                
+
+export default Carrusel;
