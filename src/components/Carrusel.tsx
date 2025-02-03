cc945405ddebe90ebd04ab1c50902cecef9784c7
Ronalm8 <ronalbuitrago93@gmail.com>
2025-02-03 17:28:12 -0500

    commit 4 cambio de icons en las imagenes del carrusel

--- src/components/Carrusel.tsx
+++ src/components/Carrusel.tsx
@@ -1,45 +1,47 @@
 
-
 function Carrusel() {
-    return (
-      <div id="carouselExample" className="carousel slide">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <a href="https://x.com/?lang=es&mx=2" className="icons twitter">
-              <i className="bi bi-twitter"></i> 
+  return (
+    <div id="carouselExample" className="carousel slide">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <div className="icons">
+            <a href="https://x.com/?lang=es&mx=2" className="twitter">
+              <i className="bi bi-twitter"></i>
             </a>
-            <a href="https://es-la.facebook.com/login/device-based/regular/login/" className="icons facebook">
-              <i className="bi bi-facebook"></i> 
+            <a href="https://es-la.facebook.com/login/device-based/regular/login/" className="facebook">
+              <i className="bi bi-facebook"></i>
             </a>
-            <a href="https://www.instagram.com/?hl=es" className="icons instagram">
+            <a href="https://www.instagram.com/?hl=es" className="instagram">
               <i className="bi bi-instagram"></i>
             </a>
-            <a href="https://github.com/" className="icons github">
+            <a href="https://github.com/" className="github">
               <i className="bi bi-github"></i>
             </a>
+          </div>
           <img src="src/components/imagen.png" className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <a href="https://es-la.facebook.com/login/device-based/regular/login/" className="icons">
-              <i className="bi bi-facebook"></i> 
-            </a>
-          <img src="src/components/radio.png" className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-          <img src="src/components/gol.png" className="d-block w-100" alt="..." />
-          </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+        <div className="carousel-item">
+          <div className="icons">
+            <a href="https://es-la.facebook.com/login/device-based/regular/login/" className="facebook">
+              <i className="bi bi-facebook"></i>
+            </a>
+          </div>
+          <img src="src/components/radio.png" className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src="src/components/gol.png" className="d-block w-100" alt="..." />
+        </div>
       </div>
-    );
-  }
-                                
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
 
 export default Carrusel;
